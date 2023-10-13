333ee79d7b00294f1e546448b2c54af107bc2982
hyeg0121 <w2218@e-mirim.hs.kr>
2023-10-13 21:42:43 +0900

    [refactor] css 모듈화

--- src/components/FirstPage.js
+++ src/components/FirstPage.js
@@ -1,6 +1,5 @@
 import React from "react";
-import '../css/style.css';
-import '../css/index.css';
+import styles from '../css/Style.module.css'; // CSS 모듈을 가져옴
 import { useNavigate } from "react-router-dom";
 
 
@@ -13,24 +12,23 @@ function App() {
   }
 
   return (
-      <div className="main"> 
-        {/* <img src="./image/bg_logo.png" alt="" className="background-img" /> */}
-        <div className="slogans">
-          <div className="app-name">COTE'IS</div>
-          <div className="app-title">당신의 코딩테스트를 위한<br />올인원 솔루션</div>
-        </div>
-        <div className="buttons">
-          <button className="login-button" onClick={goLogin}>로그인</button>
-          <button className="join-button">회원가입</button>
-          <div className="label">간편 로그인</div>
-          <div className="login-icons">
-            <div className="kakao-icon login-icon"><img src="./image/kakao_icon.png" alt=""/></div>
-            <div className="naver-icon login-icon"><img src="./image/naver_icon.png" alt="" /></div>
-            <div className="google-icon login-icon"><img src="./image/google_icon.png" alt="" /></div>
-            <div className="github-icon login-icon"><img src="./image/github_icon.png" alt="" /></div>
-          </div>
+      <div className={styles.main}>
+      <div className={styles.slogans}>
+        <div className={styles['app-name']}>COTE'IS</div>
+        <div className={styles['app-title']}>당신의 코딩테스트를 위한<br />올인원 솔루션</div>
+      </div>
+      <div className={styles.buttons}>
+        <button className={styles['login-button']} onClick={goLogin}>로그인</button>
+        <button className={styles['join-button']}>회원가입</button>
+        <div className={styles.label}>간편 로그인</div>
+        <div className={styles['login-icons']}>
+          <div className={styles['kakao-icon']}><img src="./image/kakao_icon.png" alt="" /></div>
+          <div className={styles['naver-icon']}><img src="./image/naver_icon.png" alt="" /></div>
+          <div className={styles['google-icon']}><img src="./image/google_icon.png" alt="" /></div>
+          <div className={styles['github-icon']}><img src="./image/github_icon.png" alt="" /></div>
         </div>
       </div>
+    </div>
   );
 }
 
