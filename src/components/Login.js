57017462263d1aed7dc4fc755aa3aa5d0dde1baf
hyeg0121 <w2218@e-mirim.hs.kr>
2023-10-13 23:12:09 +0900

    [feat] 로그인 버튼 누르면 로그인 요청 (서버 로그인 구현 덜 되어서 error)

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,31 +1,60 @@
 import React, { useState } from 'react';
-import styles from '../css/Login.module.css'; 
+import styles from '../css/Login.module.css';
+import axios from 'axios';  
 
 function Login() {
-    console.log('login')
-    return (
-      <div className={styles.main}>
-      <div className={styles.loginContainer}>
-        <div className="slogans">
-          <h1>COTEIS</h1>
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      const apiUrl = process.env.REACT_APP_API_URL; 
+      const response = await axios.post(`${apiUrl}/login`, { username, password });
+
+      console.log('로그인 성공:', response.data);
+    } catch (error) {
+      console.error('로그인 실패:', error);
+    }
+  };
+  return (
+    <div className={styles.main}>
+    <div className={styles.loginContainer}>
+      <div className="slogans">
+        <h1>COTEIS</h1>
+      </div>
+      <form action="/login" method='POST' className={styles.forms}>
+        <div className={styles['input-container']}>
+          <div className={styles['input-label']}>아이디</div>
+          <input 
+            type="text" 
+            className="member_id" 
+            placeholder="아이디를 입력하세요" 
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </div>
-        <form action="" className={styles.forms}>
-          <div className={styles['input-container']}>
-            <div className={styles['input-label']}>아이디</div>
-            <input type="text" className="member_id" placeholder="아이디를 입력하세요" />
-          </div>
-          <div className={styles['input-container']}>
-            <div className={styles['input-label']}>비밀번호</div>
-            <input type="password" className={styles['member_pw']} placeholder="비밀번호를 입력하세요" />
-          </div>
-        </form>
-        <div className={styles.buttons}>
-          <button className={styles.loginButton}>로그인</button>
+        <div className={styles['input-container']}>
+          <div className={styles['input-label']}>비밀번호</div>
+          <input 
+            type="password" 
+            className={styles['member_pw']} 
+            placeholder="비밀번호를 입력하세요" 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
+      </form>
+      <div className={styles.buttons}>
+        <button 
+          className={styles.loginButton}
+          onClick={handleLogin}
+        >로그인</button>
       </div>
     </div>
-    );
-  }
+  </div>
+  );
+}
   
 
 export default Login;
