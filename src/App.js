9273c00a85f126153d7e25e233fbbb68a7cd7fd0
hyeg0121 <w2218@e-mirim.hs.kr>
2023-11-05 20:34:10 +0900

    [Feat] 글 쓰기 화면 라우터

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Login from './components/Login';
 import FirstPage from './components/FirstPage';
 import Main from "./components/Main";
 import Join from "./components/Join";
-
+import Post from "./components/Post";
 
 function App() {
   return (
@@ -19,6 +19,7 @@ function App() {
         <Route path={"/login"} element={<Login />} />
         <Route path={"/main"} element={<Main />} />
         <Route path={"/join"} element={<Join />} />
+        <Route path={"/post"} element={<Post />} />
     </Routes>
   </Router>
   );
