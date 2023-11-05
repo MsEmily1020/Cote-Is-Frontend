1b80fe97515d1890a770bb7e817bb53c0a9ef58a
hyeg0121 <w2218@e-mirim.hs.kr>
2023-11-05 19:22:55 +0900

    [Feat] 배너 생성

---
+++ src/components/Banner.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from '../css/Banner.module.css';
+
+function Banner() {
+    return <div className={styles.banner}>
+
+    </div>
+}
+
+export default Banner;
