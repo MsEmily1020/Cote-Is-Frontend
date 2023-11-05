ecf8e12fb4091332d7b1914e46948f57c80553ed
hyeg0121 <w2218@e-mirim.hs.kr>
2023-11-05 20:34:29 +0900

    [Feat] 글 쓰기 목록 클릭하면 글쓰기 화면으로 이동

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,23 @@
 import React, { useState } from 'react';
 import styles from '../css/Header.module.css';
+import { useNavigate } from "react-router-dom";
 import { Icon } from '@iconify/react';
 
 function Header() {
 
+  const movePage = useNavigate();
+
+  function goToPost() {
+    movePage('/post')
+  }
   return (
     <header className={styles.header}>
       <nav>
         <div className={styles.logo}>COTEIS</div>
         <ul className={styles['nav-list']}>
-          <li className={styles['nav-item']}>
+          <li 
+            className={styles['nav-item']}
+            onClick={goToPost}>
             글쓰기
           </li>
           <li className={styles['nav-item']}>
