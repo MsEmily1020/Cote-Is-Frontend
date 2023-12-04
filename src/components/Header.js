bd417199c7d7ad32d12a05afbadb7b78672ba95c
hyeg0121 <w2218@e-mirim.hs.kr>
2023-12-04 22:06:39 +0900

    [Faet] 헤더에서 로고 클릭했을 때 메인 페이지로 이동

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,43 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import styles from '../css/Header.module.css';
-import { useNavigate } from "react-router-dom";
-import { Icon } from '@iconify/react';
+import {useNavigate} from "react-router-dom";
+import {Icon} from '@iconify/react';
 
 function Header() {
 
-  const movePage = useNavigate();
+    const movePage = useNavigate();
 
-  function goToPost() {
-    movePage('/post')
-  }
-  return (
-    <header className={styles.header}>
-      <nav>
-        <div className={styles.logo}>COTEIS</div>
-        <ul className={styles['nav-list']}>
-          <li 
-            className={styles['nav-item']}
-            onClick={goToPost}>
-            글쓰기
-          </li>
-          <li className={styles['nav-item']}>
-            게시판
-          </li>
-          <Icon icon="ph:user" className={styles["icon"]}/>
-        </ul>
-      </nav>
-    </header>
-  );
+    function goPost() {
+        movePage('/post')
+    }
+
+    function goMain() {
+        movePage('/main')
+    }
+
+    return (
+        <header className={styles.header}>
+            <nav>
+                <div
+                    className={styles.logo}
+                    onClick={goMain}
+                >COTEIS
+                </div>
+                <ul className={styles['nav-list']}>
+                    <li
+                        className={styles['nav-item']}
+                        onClick={goPost}>
+                        글쓰기
+                    </li>
+                    <li className={styles['nav-item']}>
+                        Q&A
+                    </li>
+                    <Icon icon="ph:user" className={styles["icon"]}/>
+                </ul>
+            </nav>
+        </header>
+    );
 }
-  
+
 
 export default Header;
