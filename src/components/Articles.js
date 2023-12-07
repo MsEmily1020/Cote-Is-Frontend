45a1e50605e0e8cce755ee002b30e9a4b78a9ca3
hyeg0121 <w2218@e-mirim.hs.kr>
2023-12-07 09:56:42 +0900

    [Feat] 게시글 카테고리로 분류하기

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -12,6 +12,14 @@ function Articles() {
     const [difficulties, setDifficulties] = useState([]);
     const [tests, setTests] = useState([]);
 
+    const [selectedFilters, setSelectedFilters] = useState({
+        languageNo: 0,
+        algorithmNo: 0,
+        difficultyNo: 0,
+        previoustestNo: 0,
+    });
+
+
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -68,6 +76,44 @@ function Articles() {
 
     }, []);
 
+    const handleSelectChange = (event) => {
+        const { name, value } = event.target;
+        setSelectedFilters((prevFilters) => {
+            const updatedFilters = {
+                ...prevFilters,
+                [name]: value == '0' ? 0 : +value,
+            };
+
+            filterArticles(updatedFilters);
+
+            return updatedFilters;
+        });
+    };
+
+    const filterArticles = async (filters) => {
+        try {
+            const response = await axios.get('/api/articles');
+            let filteredArticles = response.data;
+            console.log('filter', filters);
+
+            const strings = ['languageNo', 'algorithmNo', 'difficultyNo', 'previoustestNo']
+            Object.keys(filters).forEach((key, index) => {
+                const category = strings[index];
+                const filterValue = filters[key];
+                if (filterValue != 0) {
+                    filteredArticles = filteredArticles.filter((article) => article[category][category] === filterValue);
+                }
+            });
+
+            console.log(filteredArticles);
+
+            setArticles(filteredArticles);
+        } catch (error) {
+            console.error('API 요청 실패:', error);
+            setLoading(false);
+        }
+    };
+
     const handleArticleClick = articleNo => {
         // 클릭한 아티클의 번호를 사용하여 OneArticle로 네비게이션
         navigate(`/articles/${articleNo}`);
@@ -75,7 +121,45 @@ function Articles() {
 
     return (
         <div className={styles['articles-container']}>
-
+            <div className={styles['tag-container']}>
+                <select id="LanguagesSelect" onChange={handleSelectChange} name="languageNo">
+                    <option value="0">언어</option>
+                    {languages.map(language => (
+                        <option key={language.languageNo} value={language.languageNo}>
+                            {language.languageName}
+                        </option>
+                    ))}
+                </select>
+                <select id="algorithmSelect" onChange={handleSelectChange} name="algorithmNo">
+                    <option value="0">알고리즘</option>
+                    {algorithms.map(algorithm => (
+                        <option key={algorithm.algorithmNo} value={algorithm.algorithmNo}>
+                            {algorithm.algorithmName}
+                        </option>
+                    ))}
+                </select>
+                <select id="difficultySelect" onChange={handleSelectChange} name="difficultyNo">
+                    <option value="0">난이도</option>
+                    {difficulties.map(difficulty => (
+                        <option key={difficulty.difficultyNo} value={difficulty.difficultyNo}>
+                            {difficulty.difficultyName}
+                        </option>
+                    ))}
+                </select>
+                <select id="testSelect" onChange={handleSelectChange} name="previoustestNo">
+                    <option value="0">기출문제</option>
+                    {tests.map(test => (
+                        <option key={test.previoustestNo} value={test.previoustestNo}>
+                            {test.previoustestName}
+                        </option>
+                    ))}
+                </select>
+                <select id="sort" name="previoustestNo">
+                    <option value="0">최신순</option>
+                    <option value="0">오래된순</option>
+                    <option value="0">인기순</option>
+                </select>
+            </div>
             {articles.map(article => (
                 <div
                     className={styles["article-container"]}
